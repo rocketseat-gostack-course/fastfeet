54f9cd3f44f359e951d42af9a8eff38ba43f6fb6
Marcelo XP <marceloxp@gmail.com>
2020-02-26 22:07:14 -0300

    - database: Added initial migration and User model to application.

---
+++ src/app/models/User.js
@@ -0,0 +1,15 @@
+import { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init({
+      name: Sequelize.STRING,
+      password_hash: Sequelize.STRING,
+      admin: Sequelize.BOOLEAN,
+    });
+  }, {
+    sequelize
+  }
+}
+
+export default User;
---
+++ src/database/migrations/20200227004702-create-users.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(128),
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  },
+};
