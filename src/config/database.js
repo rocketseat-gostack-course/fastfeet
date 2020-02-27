d3c0760625264981562a3dcea33a16989c54d1ec
Marcelo XP <marceloxp@gmail.com>
2020-02-26 21:42:36 -0300

    - database: Initial database sequelize config.

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'fastfeet',
+  password: 'fast3689feet',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
